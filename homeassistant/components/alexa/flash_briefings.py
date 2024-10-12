02c9b898a4f7d5eb7c3ec3a3cc9fde12c6b5db6f
amalte <malte@akvist.com>
2024-10-12 17:49:51 +0200

    Bug fixes for alexa first 7 issues

--- homeassistant/components/alexa/flash_briefings.py
+++ homeassistant/components/alexa/flash_briefings.py
@@ -3,6 +3,7 @@
 import hmac
 from http import HTTPStatus
 import logging
+from typing import Any
 import uuid
 
 from aiohttp.web_response import StreamResponse
@@ -83,43 +84,32 @@ class AlexaFlashBriefingView(http.HomeAssistantView):
         for item in self.flash_briefings.get(briefing_id, []):
             output = {}
             if item.get(CONF_TITLE) is not None:
-                if isinstance(item.get(CONF_TITLE), template.Template):
-                    output[ATTR_TITLE_TEXT] = item[CONF_TITLE].async_render(
-                        parse_result=False
-                    )
-                else:
-                    output[ATTR_TITLE_TEXT] = item.get(CONF_TITLE)
+                output[ATTR_TITLE_TEXT] = self._get_template_or_value(item, CONF_TITLE)
 
             if item.get(CONF_TEXT) is not None:
-                if isinstance(item.get(CONF_TEXT), template.Template):
-                    output[ATTR_MAIN_TEXT] = item[CONF_TEXT].async_render(
-                        parse_result=False
-                    )
-                else:
-                    output[ATTR_MAIN_TEXT] = item.get(CONF_TEXT)
+                output[ATTR_MAIN_TEXT] = self._get_template_or_value(item, CONF_TEXT)
+
+            if item.get(CONF_AUDIO) is not None:
+                output[ATTR_STREAM_URL] = self._get_template_or_value(item, CONF_AUDIO)
+
+            if item.get(CONF_DISPLAY_URL) is not None:
+                output[ATTR_REDIRECTION_URL] = self._get_template_or_value(
+                    item, CONF_DISPLAY_URL
+                )
 
             if (uid := item.get(CONF_UID)) is None:
                 uid = str(uuid.uuid4())
             output[ATTR_UID] = uid
-
-            if item.get(CONF_AUDIO) is not None:
-                if isinstance(item.get(CONF_AUDIO), template.Template):
-                    output[ATTR_STREAM_URL] = item[CONF_AUDIO].async_render(
-                        parse_result=False
-                    )
-                else:
-                    output[ATTR_STREAM_URL] = item.get(CONF_AUDIO)
-
-            if item.get(CONF_DISPLAY_URL) is not None:
-                if isinstance(item.get(CONF_DISPLAY_URL), template.Template):
-                    output[ATTR_REDIRECTION_URL] = item[CONF_DISPLAY_URL].async_render(
-                        parse_result=False
-                    )
-                else:
-                    output[ATTR_REDIRECTION_URL] = item.get(CONF_DISPLAY_URL)
 
             output[ATTR_UPDATE_DATE] = dt_util.utcnow().strftime(DATE_FORMAT)
 
             briefing.append(output)
 
         return self.json(briefing)
+
+    def _get_template_or_value(self, item: dict, key: str) -> Any:
+        """Render template if available otherwise return raw value."""
+        value = item.get(key)
+        if isinstance(value, template.Template):
+            return value.async_render(parse_result=False)
+        return value
